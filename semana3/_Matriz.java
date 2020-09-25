255e4d0dbf1ba74e0b141d0ddb2d3d0a69324e02
Filipe Viana <filipe.viana@ufv.br>
2020-09-25 14:41:52 -0300

    semana 3

---
+++ semana3/_Matriz.java
@@ -0,0 +1,9 @@
+public class _Matriz {
+    public static void main(String[]args){
+        for(int i = 0; i<999;i++){
+            for(int y = 0;y<999;y++){
+                System.out.printf("Percorrendo a linha %d e coluna %d\n\n",i,y);
+            }
+        }
+    }
+}
