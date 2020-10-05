38361ad0b41c6a6eb16dc0e18c28ebac9689b401
Filipe Viana <filipe.viana@ufv.br>
2020-10-04 23:25:33 -0300

    sobrecarga de metodo

---
+++ semana5/Metodo.java
@@ -0,0 +1,11 @@
+public class Metodo{
+    public void Somar(int a; int b){
+        System.out.printf("Resultado: ",a+b);
+    }
+    public void Somar(int a; int b, int c){
+        System.out.printf("Resultado: ",a+b+c);
+    }
+    public void Somar(int a; int b, int c, int d){
+        System.out.printf("Resultado: ",a+b+c+d);
+    }
+}
